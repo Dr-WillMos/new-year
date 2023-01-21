65c32492513e24ba5520d97e6eb546e862e9c8c6
Willmo <72198229+Dr-WillMos@users.noreply.github.com>
2023-01-21 18:26:19 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 var myvideo = document.getElementById("video");
-var str = '所求皆所愿<br>所行皆坦途<br>多喜乐<br>长安宁';
+var str = '身体健康<br>学业有成<br>心想事成<br>天天开心';
 var i = 0;
 var divTyping = document.getElementById('zhuf');
 function typing(){
